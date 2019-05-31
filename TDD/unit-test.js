68ec71e865bb537f5a82999d10c850f33be786f4
kuanzi <997128105@qq.com>
2019-05-31 10:28:35 +0800

    tdd2
    tdd2

--- TDD/unit-test.js
+++ TDD/unit-test.js
@@ -7,4 +7,11 @@ describe('taxi fee', function () {
         let expected = 6;
         expect(summary).toEqual(expected);
     });
+
+    it('more than 2km and no parking' , function() {
+        let inputs = {distance:7,parkTime:0};
+        let summary = main(inputs);
+        let expected = 10;
+        expect(summary).toEqual(expected);
+    });
 });
--- main/main.js
+++ main/main.js
@@ -1,6 +1,6 @@
 module.exports = function main(inputs) {
     price =  taxi(inputs['distance'], inputs['parkTime']);
-    return price;
+    return Math.round(price);
 };
 
 function taxi(distance , parkTime){
@@ -12,22 +12,21 @@ function taxi(distance , parkTime){
     4. 停车等待时加收每分钟0.25元；
     5. 最后计价的时候司机会四舍五入只收整块钱。    
     Input params：
-    distince:公里数，wait_time:等待时间
+    distance:公里数，wait_time:等待时间
     Output:
     price:最终支付价格
     */
-
     var distance_money =0;  //路程费用
-
-
     if(distance <= 2){
         distance_money = 6 ; //2公里以内 起步价6元
+    }else if(distance > 2 && distance <= 8){
+        distance_money = 6 + (distance - 2) * 0.8;
     }
 
     var wait_money = 0; // 等待费用
-    console.log(distance_money)
-    console.log(wait_money)
 
+    console.log(distance_money);
+    console.log(wait_money);
     return wait_money + distance_money;
 }
 //测试
