9568d7a96bff8c55951fc2281b2509c2430b3ef8
kuanzi <997128105@qq.com>
2019-05-31 10:37:02 +0800

    final
    final

--- run-tests.js
+++ run-tests.js
@@ -1,7 +1,6 @@
 const Jasmine = require('jasmine');
 const jasmine = new Jasmine();
 
-// jasmine.loadConfigFile('spec/support/jasmine.json');
-jasmine.loadConfigFile('TDD/support/jasmine.json');
+jasmine.loadConfigFile('spec/support/jasmine.json');
 
 jasmine.execute();
